1a4fc5990b5beb916bf11d9d8b73a0cafb4c2148
yanchak01 <a.yanchak01@gmail.com>
2019-05-26 22:59:32 +0300

    starting of ordercontroller

---
+++ WebCustomerApp/Controllers/OrderUserController.cs
@@ -0,0 +1,21 @@
+﻿using BAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class OrderUserController:Controller
+    {
+
+        private readonly IOrderUserManager orderUserManager;
+        public OrderUserController(IOrderUserManager orderUserManager)
+        {
+            this.orderUserManager = orderUserManager;
+        }
+
+
+    }
+}
