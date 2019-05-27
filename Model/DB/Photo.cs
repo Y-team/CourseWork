ccbd3b76be23cfe82a297f77942f6163e9f3d634
yanchak01 <a.yanchak01@gmail.com>
2019-05-27 19:26:35 +0300

    little Changes

--- Model/DB/Photo.cs
+++ Model/DB/Photo.cs
@@ -11,20 +11,6 @@ namespace WebCustomerApp.Models
        public int CommodityId { get; set; }
        public  Commodity Commodity { get; set; }
        public byte[] Paint { get; set; }
-      /* private byte[] ConvertImageToByteArray(System.Drawing.Image imageToConvert,
-           System.Drawing.Imaging.ImageFormat formatOfImage)
-       {
-           byte[] Ret;
-           try
-           {
-               using (MemoryStream ms = new MemoryStream())
-               {
-                   imageToConvert.Save(ms, formatOfImage);
-                   Ret = ms.ToArray();
-               }
-           }
-           catch (Exception) { throw; }
-           return Ret;
-       }*/
+       public string Name { get; set; }
     }
 }
---
+++ WebCustomerApp/Controllers/PhotoController.cs
@@ -0,0 +1,26 @@
+﻿using BAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCustomerApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PhotoController:Controller
+    {
+        private readonly IPhotoManager photoManager;
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+       
+
+    }
+}
